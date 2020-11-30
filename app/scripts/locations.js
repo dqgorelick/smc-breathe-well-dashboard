4f4c2d813542b2267d9cf54359b7d30a17f22ea8
dqgorelick <dqgorelick@gmail.com>
2020-11-30 18:21:07 -0500

    initial commit of dashboard

---
+++ app/scripts/locations.js
@@ -0,0 +1,168 @@
+var SensorLocations = {
+  'type': 'geojson',
+  'data': {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        type: 'Feature', properties: {
+          id: 1, description: "Benjamin Banneker Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.6758535, 38.3297214] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 2, description: "Captain Walter Francis Duke Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.6363073, 38.3144394] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 3, description: "Dynard Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.7591032, 38.3140916] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 4, description: "Evergreen Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.5633583, 38.2993082] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 5, description: "George Washington Carver Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4798641, 38.2497056] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 6, description: "Green Holly Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4825953, 38.2944748] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 7, description: "Greenview Knolls School Age Center", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4930985, 38.2800766] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 8, description: "Hollywood Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.5421879, 38.3373834] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 9, description: "Leonardtown Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.6392651, 38.2967609] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 10, description: "Lettie M Dent Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.7676672, 38.4743607] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 11, description: "Lexington Park Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4578451, 38.2600484] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 12, description: "Mechanicsville Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.7545612, 38.4531739] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 13, description: "Oakville Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.6464347, 38.3945035] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 14, description: "Park Hall Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4518582, 38.2240115] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 15, description: "Piney Point Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.5360991, 38.1667233] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 16, description: "Ridge Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.3655162, 38.1105587] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 17, description: "Town Creek Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4911319, 38.302936] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 18, description: "White Marsh Elementary School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.7762817, 38.4670458] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 19, description: "Margaret Brent Middle School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.7161651, 38.3799173] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 20, description: "Spring Ridge Middle School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4012935, 38.2122472] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 21, description: "Esperanza Middle School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4889685, 38.2926091] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 22, description: "Leonardtown Middle School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.6122046, 38.2738933] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 23, description: "Chesapeake Charter Public School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4929258, 38.2429941] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 24, description: "Chopticon High School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.7063079, 38.3595688] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 25, description: "Great Mills High School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4874175, 38.2474355] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 26, description: "Leonardtown High School", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.61129, 38.2696812] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 27, description: "Fairlead Academy", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.4945314, 38.2405204] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 28, description: "Dr. James A. Forrest Career & Technology Center", purpleAirPlanned: true, purpleAirInstalled: false, AeroqualSensor: false, EPA: false
+        }, geometry: { type: 'Point', coordinates: [-76.6117031, 38.2716109] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 29, description: "St. Mary's County Sheriff's Office", purpleAirPlanned: false, purpleAirInstalled: false, AeroqualSensor: true, EPA: 'SOMETIMES'
+        }, geometry: { type: 'Point', coordinates: [-76.6288716, 38.3046296] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 30, description: "District 1 Station", purpleAirPlanned: false, purpleAirInstalled: false, AeroqualSensor: true, EPA: 'SOMETIMES'
+        }, geometry: { type: 'Point', coordinates: [-76.7763099, 38.478134] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 32, description: "District 4 Station", purpleAirPlanned: false, purpleAirInstalled: false, AeroqualSensor: true, EPA: 'SOMETIMES'
+        }, geometry: { type: 'Point', coordinates: [-76.4636743, 38.2616615] }
+      },
+      {
+        type: 'Feature', properties: {
+          id: 33, description: "Health Department (Leonardtown)", purpleAirPlanned: false, purpleAirInstalled: false, AeroqualSensor: true, EPA: 'SOMETIMES'
+        }, geometry: { type: 'Point', coordinates: [-76.6372424, 38.2993353] }
+      },
+    ]
+  }
+}
